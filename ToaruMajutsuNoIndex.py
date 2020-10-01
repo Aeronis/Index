7d719f6144fb1afa24b700fe87f8e2cb661a12e7
repl.it user <replituser@example.com>
2020-10-01 15:07:58 +0000

    Index Guide

---
+++ ToaruMajutsuNoIndex.py
@@ -0,0 +1,12 @@
+print("ZZZ") #Displays text for the user
+YYY = input() #Expects the user to enter a value and XXX is he variable for that value
+print("Blah blah blah", YYY) #Allows you to input the value into dialogue
+XXX = int(input()) #Reads a user input as a number
+WWW = float(input)() #Floats decimal numbers or numbers for scientific notation
+#-----#
+print("\n") #Skips a line
+print(""" / """) #Allows the dialogue to span dialogue accross multiple lines
+print("\t") #Adds a tab into dialogue
+print("\n\t") #Mix prenotes together
+print('"XXX"') #Use single quotes to allow spoken dialogue
+#-----#
