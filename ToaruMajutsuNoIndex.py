41a944a5352ac96dd8fa9371f6b953de110c5de5
repl.it user <replituser@example.com>
2020-10-06 09:03:17 +0000

    Index and scenarios

--- ToaruMajutsuNoIndex.py
+++ ToaruMajutsuNoIndex.py
@@ -2,7 +2,7 @@ print("ZZZ") #Displays text for the user
 YYY = input() #Expects the user to enter a value and XXX is he variable for that value
 print("Blah blah blah", YYY) #Allows you to input the value into dialogue
 XXX = int(input()) #Reads a user input as a number
-WWW = float(input)() #Floats decimal numbers or numbers for scientific notation
+WWW = float(input()) #Floats decimal numbers or numbers for scientific notation
 #-----#
 print("\n") #Skips a line
 print(""" / """) #Allows the dialogue to span dialogue accross multiple lines
@@ -10,10 +10,17 @@ print("\t") #Adds a tab into dialogue
 print("\n\t") #Mix prenotes together
 print('"XXX"') #Use single quotes to allow spoken dialogue
 #-----#
-if (len(YYY) > 10): #Len means 'length' of the input value
+if (len(XXX) > 10): #Len means 'length' of the input value
   print("This value is very long")
-elif (len(YYY) > 6): #'Else If' is a way to add other declarable values to an if statement
+elif (len(XXX) > 6): #'Else If' is a way to add other declarable values to an if statement
   print("This value is longer")
 else: #Only one else per if statement
   print("This value is shorter") #Without an 'else' after the if code will flow down again
+#-----#
+for value in range(1, 10, 1): #Lists out numbers X to X in incriments of X
+  #1-10 is range and 1 is incriment
+  print(value)
+while (iterations < XXX): #Will print XXX Y times, depending on the user input
+  Print("Word")
+  iterations = iterations + 1 #Control variable
 #-----#
---
+++ scenarios/forloop.py
@@ -0,0 +1,8 @@
+print("Please enter your name")
+name = input()
+
+for character in name:
+  print(character)
+
+  for value in range(1, 10, 1): #1-10 is range and 1 is incriment
+  print(value)
