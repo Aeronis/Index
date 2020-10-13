6bc274726ae935d2936f0842c9646eed24b22720
repl.it user <replituser@example.com>
2020-10-13 09:09:11 +0000

    Index

---
+++ scenarios/AreaOfATriangle.py
@@ -0,0 +1,11 @@
+#Program to calculate the area of a triangle
+def calculate_area(base, height): #Definition of the function
+  area = 0.5 * base * height
+  return area
+  #call function
+  print( calculate_area(10, 5) )
+
+  def run()
+  area = calculate_area(7,3)
+  print(area)
+run()
